296c58bdeb620b1d57d91efbde80be5c1a4ef169
Eric Mehiel <emehiel@calpoly.edu>
2024-01-10 14:24:07 -0800

    import Animal to show custom module use

--- Code_5_0_Modules.py
+++ Code_5_0_Modules.py
@@ -15,3 +15,16 @@ from math import pi as PI
 
 math.sinh(1)
 sinh(2)
+
+# Import the code that has the Animal Class definitions
+# Note, this is terrible naming, but we can make it work
+import Code_4_3_classes_poly_2 as ans
+
+animal1 = ans.Animal()
+
+piper = ans.Dog('Piper', 'Ball')
+daisy = ans.Dog('Daisy', 'Rope')
+
+dogs = [piper, daisy]
+for d in dogs:
+    print(d.name)
