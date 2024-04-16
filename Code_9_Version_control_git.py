3c1c12170e482bb07030048a7a939b3b7be238ed
Eric Mehiel <emehiel@calpoly.edu>
2024-04-15 22:18:55 -0700

    Update to version control code

--- Code_9_Version_control_git.py
+++ Code_9_Version_control_git.py
@@ -1,1 +1,5 @@
-# to do
+# This is a sample file used to show source control using GitHub and VS Code
+# This folder is under source control
+# Any changes will be tracked by Git in VS Code
+
+print('Hello World!')
